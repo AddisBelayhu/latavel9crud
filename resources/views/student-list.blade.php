07cc06647086f03c8cb696b11be2e94fb6e90bcf
AddisBelayhu <addisgithub@gmail.com>
2023-06-14 14:47:14 +0300

    accessing data

--- resources/views/student-list.blade.php
+++ resources/views/student-list.blade.php
@@ -3,9 +3,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>student list form</title>
 </head>
 <body>
-    <h4>Student List</h4>
+<div class="container" style="margin-top:60px">
+    <div class="row">
+        <div class="col-md-12">
+            <h3>Student List</h3><br>
+            <table class="table">
+              <thead><tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Address</th>    
+                    <th>Action</th>            
+              </tr></thead>
+            <tbody>
+                 @php
+                  $i=1 ;
+                 @endphp
+
+                 @foreach ($data as $studata)
+            
+                <tr>
+                    <td>{{$i++}}</td>
+                    <td>{{$studata->name}}</td>
+                    <td>{{$studata->email}}</td>
+                    <td>{{$studata->phone}}</td>
+                    <td>{{$studata->address}}</td>
+                    <td>Edit | Delete</td>
+                </tr>
+                @endforeach 
+            </tbody>
+            
+        </table>
+                         
+
+        </div>
+    </div>
+</div>
+
+    
 </body>
 </html>
