775ab5d21d30b0ab52b086455a28cc84eb97090c
AddisBelayhu <addisgithub@gmail.com>
2023-06-14 11:21:12 +0300

    com

--- app/Http/Controllers/studentController.php
+++ app/Http/Controllers/studentController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\student;
 
 class studentController extends Controller
 {
-    //
+    //accessing all data from the databse
+    public function index(){
+        $data = student::get();
+        return $data;
+    }
 }
